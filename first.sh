a5df36a2a1fabbf4fd9efec9ffdb680115ddf22c
Rodriguekmr <kmrodrigue7@gmail.com>
2024-11-16 19:09:52 +0100

    commit for first.sh

---
+++ first.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# x=8
+# y=7
+# z=11
+# if [[ $x -gt $y ]]; then
+#   echo "$x > $y"
+# elif [[ $y -gt $z ]]; then 
+#   echo "$y > $z"
+# else 
+#   echo "$z is the highest number."
+# fi
+
+i=1
+while [ $i -le 10 ];
+do
+  echo "$i"
+  ((i+=1))
+done
+for i in {90..100}
+do
+  echo "$i"
+done
